541f84b931f1ad76604089376ed71594541da118
user.name <user.email>
2023-02-01 22:34:27 +0100

    fix dump_data_partition.sh to work with Sliver ramdisk and newer macOS versions

--- dump_data_partition.sh
+++ dump_data_partition.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SSHOPTS="-p 2222 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null"
+SSHOPTS="-p 2222 -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oHostKeyAlgorithms=+ssh-dss"
 
 UDID=`ssh $SSHOPTS root@localhost "./device_infos udid"`
 
@@ -16,4 +16,4 @@ OUT=$UDID/data_$DATE.dmg
 
 echo "Dumping data partition in $OUT ..."
 
-ssh $SSHOPTS root@localhost "dd if=/dev/rdisk0s2s1  bs=8192 || dd if=/dev/rdisk0s1s2  bs=8192" > $OUT
+ssh $SSHOPTS root@localhost "cat /dev/rdisk0s2s1 || cat /dev/rdisk0s1s2" > $OUT
