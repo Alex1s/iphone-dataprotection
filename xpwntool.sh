c2b0e88112343efbf7a6809a73425b4925755500
user.name <user.email>
2023-02-01 22:09:58 +0100

    add pre-compiled xpwntool and shell script to convert patched kernel to Img3

---
+++ xpwntool.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env sh
+./xpwntool /Applications/Sliver.app/Contents/Resources/Master/iphone4gsm/kernelcache.patched /Applications/Sliver.app/Contents/Resources/Master/iphone4gsm/kernelcache.patched.img3 -t /Applications/Sliver.app/Contents/Resources/Master/iphone4gsm/kernelcache
